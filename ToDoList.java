1ef780fcbee6a53eeb79b4d9e2831724c15e1df1
jhj010311 <57277646+jhj010311@users.noreply.github.com>
2025-04-25 14:21:43 +0900

    Create ToDoList.java

---
+++ ToDoList.java
@@ -0,0 +1,8 @@
+public class ToDoList {
+  public void main(String[] args) {
+    System.out.println("To-do List");
+    System.out.println("1. 교육과정 수료");
+    System.out.println("2. 포트폴리오 제작");
+    System.out.println("3. 취업");
+  }
+}
